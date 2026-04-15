d7f1c89e6f90cd1f9d20c60264fee3b4176f9ce0
Gabriel Pires <gbnp0259934@gmail.com>
2026-04-14 21:05:21 -0300

    feat: Implementa controller login

--- petCareFrontEnd/src/Controller/HomeController.php
+++ petCareFrontEnd/src/Controller/HomeController.php
@@ -8,11 +8,22 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/home', name: 'app_home', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('landing_page/index.html.twig');
     }
+
+    #[Route('/login', name: 'app_login', methods: ['GET'])]
+    public function getLogin(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/cadastro', name: 'app_cadastro', methods: ['POST'])]
+    public function getCadastro(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
 }
